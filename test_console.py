2c75f31d008039aa910f82e4e5019e22159b29c6
Muideen7 <olayeyeayomide2000@gmail.com>
2023-02-11 22:50:44 +0100

    test_console.py

---
+++ test_console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines unittests for console.py.
+
+Unittest classes:
+    TestHBNBCommand_prompting
+    TestHBNBCommand_help
+    TestHBNBCommand_exit
+    TestHBNBCommand_create
+    TestHBNBCommand_show
+    TestHBNBCommand_all
+    TestHBNBCommand_destroy
+    TestHBNBCommand_update
+"""
+import os
+import sys
+import unittest
+from models import storage
+from models.engine.file_Storage import FileStorage
+from console import HBNBCommand
+from io import StringIO
+from unittest.mock import patch 
+
+class TestHBNBCommand_prompting(unittest.TestCase):
+    """Unittest for testing the prompting of the HBNB command interpreter."""
+
+    def test_prompt_string(self):
+        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
