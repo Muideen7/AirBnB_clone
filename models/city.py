2a6a7344b94a4dc72596a297278d4989a205b3df
Olayeye Muideen Ayomide <olayeyeayomide2000@gmail.com>
2023-02-12 21:13:36 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -2,16 +2,14 @@
 """
 Defines the City class
 """
+
 from models.base_model import BaseModel
 
-
 class City(BaseModel):
-    """Represent a city
-
-    Attributes:
-        state_id (str): The state id.
-        name (str): The name of the city
-
+    """Represent a City
+       Attributes:
+           state_id (str): The id of the state
+           name (str): The name of the state
     """
 
     state_id = ""
